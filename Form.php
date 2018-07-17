88229cd4ae053196898804b22e6fb4ae5ea24bfc
Kiran Naz <kiranaz_123@outlook.com>
2018-07-17 14:21:37 +0500

    Styling added in forms

--- Form.php
+++ Form.php
@@ -1,7 +1,17 @@
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>Form</title>
+
+    <script type="text/javascript" src="logics.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    
+    <link href="https://fonts.googleapis.com/css?family=Oleo+Script:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Teko:400,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <title>Form</title>
  </head>
 
  <style>
@@ -19,58 +29,58 @@
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
- 	if (empty($_POST["name"])) {
- 		$nameErr = "Name is Required";
- 	} else{
- 		$name = test_input($_POST["name"]);
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is Required";
+    } else{
+        $name = test_input($_POST["name"]);
 
- 		if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
- 			$nameErr = "Only letters and white space allowed";
- 		}
- 	}
-
-
- 	if (empty($_POST["Email"])) {
- 		$EmailErr = "Email is Required";
- 	} else{
- 		$Email = test_input($_POST["Email"]);
-
- 		if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
- 			$EmailErr="Invalid Email Format";
- 		}
- 	}
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
 
 
- 	if (empty($_POST["comment"])) {
- 		$comment ="";
- 	} else{
- 		$comment= test_input($_POST["comment"]);
- 	}
+    if (empty($_POST["Email"])) {
+        $EmailErr = "Email is Required";
+    } else{
+        $Email = test_input($_POST["Email"]);
+
+        if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+            $EmailErr="Invalid Email Format";
+        }
+    }
 
 
- 	if (empty($_POST["Phone"])) {
- 		$PhoneErr = "Phone number is Required";
- 	} else{
- 		$Phone = $_POST["Phone"];
-
- 		if (!(strlen($Phone)==11) ) {
- 			$PhoneErr="Invalid Phone Number";
- 		}
- 	}
-
- 	}
+    if (empty($_POST["comment"])) {
+        $comment ="";
+    } else{
+        $comment= test_input($_POST["comment"]);
+    }
 
 
- 	
+    if (empty($_POST["Phone"])) {
+        $PhoneErr = "Phone number is Required";
+    } else{
+        $Phone = $_POST["Phone"];
+
+        if (!(strlen($Phone)==11) ) {
+            $PhoneErr="Invalid Phone Number";
+        }
+    }
+
+    }
 
 
- 	function test_input($data){
- 		$data = trim($data);
- 		$data = stripslashes($data);
- 		$data = htmlspecialchars($data);
- 		return $data;
+    
 
- 	}
+
+    function test_input($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+
+    }
 
 
  ?>
@@ -78,16 +88,16 @@
  <?php
 //____________________________________- MYSQL _________________________________
 
- 	if(isset($_POST['submit']) && $nameErr=="" && $EmailErr=="" && $PhoneErr=="" ) {
- 	
+    if(isset($_POST['submit']) && $nameErr=="" && $EmailErr=="" && $PhoneErr=="" ) {
+    
 
- 	$conn = mysqli_connect('localhost','root','','pizzeria');
+    $conn = mysqli_connect('localhost','root','','pizzeria');
 
- 	if (!$conn) {
+    if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());}
 
     if (!mysqli_select_db($conn,'pizzeria')) {
-    	echo "Database Not Selected";
+        echo "Database Not Selected";
     }
 
     $Name = $_POST["name"];
@@ -98,49 +108,59 @@
     $sql="INSERT INTO User(Name,Email,Phone,Address) VALUES('$Name','$Email','$Phone','$Comment')";
 
     if (!mysqli_query($conn,$sql)) {
-    	echo 'Not inserted';
+        echo 'Not inserted';
     }else{
-    	echo 'Inserted';
+        echo 'Inserted';
     }
 
     }
-
 
 
  ?>
- 
- 	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
 
 
- 		Name:	<input type="text" name="name" value="<?php echo $name; ?>">
- 		<span class="error">* <?php echo $nameErr;?> </span>
- 		<br>
- 		Email:	<input type="text" name="Email" value="<?php echo $Email; ?>">
- 		<span class="error">* <?php echo $EmailErr;?> </span>
- 		<br>
- 		Phone#:	<input type="text" name="Phone" value="<?php echo $Phone; ?>">
- 		<span class="error">* <?php echo $PhoneErr; ?> </span>
- 		<br> <br>
- 		Address:<textarea name="comment" rows="5" cols="40">
- 			<?php echo $comment; ?>
- 		</textarea>
- 		<br><br>
- 		<input  type="submit" name="submit" value="submit">		
 
- 	</form>
+ <div id="contact">
+    <div class="section-content">
+        <h1 class="section-header">Make Order</h1>
+    </div>
+    <div class="contact-section">
+        <div class="container">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" >
+        <div class="col-md-6 form-line">
+        <div class="form-group">
+        <label for="exampleInputUsername">Your Name:</label>
+        <input type="text" class="form-control" name="name" value="<?php echo $name; ?>" placeholder="Enter Name">
+        <span class="error"><?= $nameErr?></span>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail">Email Address</label>
+        <input type="text" class="form-control" name="Email" value="<?php echo $Email; ?>" placeholder="Enter Email ID">
+        <span class="error"><?= $EmailErr;?></p> </span>
+        </div>
+        <div class="form-group">
+        <label for="telephone">Mobile No.</label>
+            <input type="text" class="form-control" name="Phone" value="<?php echo $Phone; ?>" placeholder="Enter 11-digit mobile no.">
+        <span class="error"><?=$PhoneErr; ?></p> </span>
+        </div>
+    </div>
+    
+    <div class="col-md-6">
+        <div class="form-group">
+           <label for ="description"> Address</label>
+        <textarea class="form-control" name="comment" placeholder="Enter Your Complete Address">
+            <?php echo $comment; ?>
+        </textarea>
+    </div>
+    <div>
+            <button type="submit" name="submit" value="submit" class="btn btn-default submit">  Order</button>
+                        </div>
+                         <p class="text-warning">Order must of Rs 500 or more.</p>
+                        
+                    </div>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $Email;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $Phone; 
-
-?>
-
- 
- </body>
- </html>
+    </form>
+    </div>
+</div>
+</body>
+    <html>
