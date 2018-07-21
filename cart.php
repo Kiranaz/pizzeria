1418421147232bb4983e4ca0c66a1d67a5aa7a2e
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2018-07-21 13:46:41 +0500

    cart change

--- cart.php
+++ cart.php
@@ -1,15 +1,50 @@
-<?php include 'header.php'; ?>
-<?php
-  session_start();
-  $items = $_SESSION['items'];
- for($i = 0; $i < sizeof($items) ; $i++ ){
- 	echo "id : ". $items[$i]->id ."<br>";
-  	echo "qty : ". $items[$i]->qty ."<br>";
+<?php include 'header.php';?>
+	<title>Cart</title>
+</head>
+<body>
 	
- }
- ?>
 
 
 
 
-<?php include 'footer.php'; ?>
+<?php
+session_start();
+ $items = $_SESSION['items'];
+for($i = 0; $i < sizeof($items) ; $i++ ){
+	echo "id : ". $items[$i]->id ."<br>";
+ 	echo "qty : ". $items[$i]->qty ."<br>";
+}
+?>
+
+
+	<div class="order_table_div">
+		
+
+	<table class="order_history">
+
+<thead > 
+
+<tr style=" color: black; ">
+<th width="40%" class="table_head_css">Name</th>
+<th width="20%" class="table_head_css">Size</th>
+<th width="20%" class="table_head_css">Quantity</th>
+<th width="18%" class="table_head_css">price</th>
+<th width="2%"  class="table_head_css">Remove</th>
+</tr>
+</thead>
+
+<tbody id="table_body">
+
+
+</tbody>
+
+</table>
+
+</div>
+
+<button onclick="cart_table1()">Try it haseeb</button>
+
+
+
+</body>
+</html>	
--- mainpage.php
+++ mainpage.php
@@ -33,7 +33,7 @@
 
 
 
-<?php include 'footer.php'; ?>
+
 
 
 
